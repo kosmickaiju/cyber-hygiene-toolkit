98426b3758dcb8719d785f477a78d7890794c8ac
Luca Laureno <luca.laureno@gmail.com>
2025-06-10 11:07:37 -0400

    finish port scan, updated main, and added suggestions to end of scan summary

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description="Cyber Hygiene Toolkit")
     parser.add_argument('--check-password', action='store_true', help="Check if a password has been leaked")
-    parser.add_argument('--scan-ports', action='store_true', help="Check for open ports")
+    parser.add_argument('--scan-ports', action='store_true', help="Check for open ports on localhost")
+    parser.add_argument('--summary', action='store_true', help="Run all hygiene checks and print results")
     
     args = parser.parse_args()
 
@@ -12,7 +13,34 @@ def main():
         password_checker.password_check()
     if args.scan_ports:
         from scanners import port_scanners
-        port_scanners.port_scan()
-        
+        port_scanners.run_scan()
+    if args.summary:
+        run_summary()
+
+# prints results for all tests and provides suggestions on how to improve cyber hygiene - currently password checker, port scanner
+def run_summary():
+    from scanners import password_checker, port_scanners
+    # run password check
+    print(f'✰✰✰✰✰✰ Your Cyber Hygiene Summary ✰✰✰✰✰✰ \n')
+    password = input("Enter your password to see if it's been pwned... ")
+    count = password_checker.check_pwned_password(password)
+    if count:
+        print(f'Uh oh! This password has been found {count} times in data breaches.')
+    else:
+        print(f'Good news! This password was not found in any known breaches... yet.')
+    #run port scan
+    open_ports = port_scanners.run_scan()
+    if open_ports:
+        print(f"\n {len(open_ports)} open port(s) found: {open_ports}")
+    else:
+        print("\n All clear! No open ports found.")
+    # print personalized suggestions based on scan results
+    if count or open_ports:
+        print(f'✰✰✰✰✰✰ Your Personalized Cyber Hygiene Suggestions ✰✰✰✰✰✰ \n')
+        if count:
+            print(f'Looks like one of your passwords got pwned! Check out this site about good password practices to avoid being pwned again: https://www.cisa.gov/secure-our-world/use-strong-passwords')
+        if open_ports:
+            print(f'It seems that you have a few ports open on your device. No worries though - these are all essential ports that need to be open to the Internet for you to use your computer. However, make sure that you have a firewall installed and keep your software/operating system updated to avoid potential attacks!')
+
 if __name__ == "__main__":
     main()
--- scanners/port_scanners.py
+++ scanners/port_scanners.py
@@ -1,3 +1,4 @@
+from tqdm import tqdm
 import socket
 
 def scan_port(port):
@@ -5,3 +6,18 @@ def scan_port(port):
       s.settimeout(0.5)
       result = s.connect_ex(('127.0.0.1', port)) 
       return result == 0
+   
+# running the actual scan of each port from 20 to 1024
+def run_scan(start = 20, end = 1024):
+   print(f'Scanning ports {start} - {end} on localhost...')
+   open_ports = []
+   for port in tqdm(range(start, end + 1), desc = 'Scanning', unit = 'port'):
+      if scan_port(port):
+         open_ports.append(port)
+   if open_ports:
+      print('Open ports found: ')
+      for port in open_ports:
+         print(f'- Port {port}')
+   else:
+      print(f'All clear! No open ports found.')
+   return open_ports
