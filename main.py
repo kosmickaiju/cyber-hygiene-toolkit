708f8c84a55b25c59519b42654850728d7719525
Luca Laureno <luca.laureno@gmail.com>
2025-06-09 23:48:41 -0400

    added port scan option

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description="Cyber Hygiene Toolkit")
     parser.add_argument('--check-password', action='store_true', help="Check if a password has been leaked")
+    parser.add_argument('--scan-ports', action='store_true', help="Check for open ports")
     
     args = parser.parse_args()
 
     if args.check_password:
         from scanners import password_checker
         password_checker.password_check()
-
+    if args.scan_ports:
+        from scanners import port_scanners
+        port_scanners.port_scan()
+        
 if __name__ == "__main__":
     main()
